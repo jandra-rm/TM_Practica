52d41f4ef2deb47dc09a5c61697a5ec718e6f329
jandra-rm <alejandraribasmari@gmail.com>
2021-03-23 13:37:02 +0100

    Intento Carousel fallido :)

--- ejemplo/js/scripts.js
+++ ejemplo/js/scripts.js
@@ -64,6 +64,34 @@
         $(this).removeClass("floating-label-form-group-with-focus");
       });
     });
-  
+
+    //Carousel
+    $(document).ready(function(){
+      // Activate Carousel
+      $("#myCarousel").carousel();
+        
+      // Enable Carousel Indicators
+      $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+      });
+      $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+      });
+      $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+      });
+      $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+      });
+        
+      // Enable Carousel Controls
+      $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+      });
+      $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+      });
+    });
+
   })(jQuery); // End of use strict
   
