04c039bf7c9359c00882aa262a467e2a198ecca7
jandra_rm <alejandraribasmari@gmail.com>
2021-04-03 19:56:23 +0200

    Prueba javascript

--- js/scripts.js
+++ js/scripts.js
@@ -66,32 +66,88 @@
   });
 
 
-//Carousel
-$(document).ready(function () {
-  // Activate Carousel
-  $("#myCarousel").carousel();
+  //Carousel
+  $(document).ready(function () {
+    // Activate Carousel
+    $("#myCarousel").carousel();
 
-  // Enable Carousel Indicators
-  $(".item1").click(function () {
-    $("#myCarousel").carousel(0);
-  });
-  $(".item2").click(function () {
-    $("#myCarousel").carousel(1);
-  });
-  $(".item3").click(function () {
-    $("#myCarousel").carousel(2);
-  });
-  $(".item4").click(function () {
-    $("#myCarousel").carousel(3);
+    // Enable Carousel Indicators
+    $(".item1").click(function () {
+      $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function () {
+      $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function () {
+      $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function () {
+      $("#myCarousel").carousel(3);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function () {
+      $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function () {
+      $("#myCarousel").carousel("next");
+    });
   });
 
-  // Enable Carousel Controls
-  $(".left").click(function () {
-    $("#myCarousel").carousel("prev");
-  });
-  $(".right").click(function () {
-    $("#myCarousel").carousel("next");
-  });
-});
 
-  }) (jQuery); // End of use strict
+
+  // Prueba de imágenes con JSON
+
+  function myFunction(arr) {
+    var i;
+    for (i = 0; i < arr[0].length; i++) {
+      var a = document.createElement('div');
+      a.className = 'col-md-6 col-lg-4 mb-5';
+
+      var b = document.createElement('div');
+      b.className = 'portfolio-item mx-auto';
+      b.attr("data-toggle", "modal");
+      b.attr("data-target", "#portfolioModal1");
+      a.appendChild(b);
+
+      var img = document.createElement('img');
+      img.src = arr[0][i];
+      b.appendChild(img);
+
+      var c = document.createElement('div');
+      c.className = 'portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100';
+      b.appendChild(c);
+
+      var d = document.createElement('div');
+      d.className = 'portfolio-item-caption-content text-center text-white sport';
+      d.innerHTML = arr[1][i].display;
+      c.appendChild(d);
+
+      document.getElementById("catalogo").appendChild(a);
+    }
+  }
+
+  $(document).addEventListener('DOMContentLoaded', function () {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "datos.json";
+
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        myFunction(myArr);
+      }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+  }, false);
+
+
+
+
+
+
+
+
+})(jQuery); // End of use strict
