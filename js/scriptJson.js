cac84b683d236129362d378391329625609cbb60
jandra_rm <alejandraribasmari@gmail.com>
2021-04-06 18:22:31 +0200

    cloneNode

--- js/scriptJson.js
+++ js/scriptJson.js
@@ -16,32 +16,59 @@ request.onreadystatechange = function () {
 
 function showSports(d) {
   const sports = d['dadesPropies']['esports'];
-  for (var i = 0; i < sports.length; i++) {
-    var a = document.createElement("div");
-    a.classList.add('col-md-6', 'col-lg-4', 'mb-5');
+  const imatges = d['imatges'];
+  var i=0;
 
-    var b = document.createElement("div");
-    b.classList.add('portfolio-item', 'mx-auto');
+
+  //Primera iteración
+  var a = document.createElement("div");
+  a.classList.add('col-md-6', 'col-lg-4', 'mb-5');
+
+  var b = document.createElement("div");
+  b.classList.add('portfolio-item', 'mx-auto');
+  //b.attr("data-toggle", "modal");
+  //b.attr("data-target", "#portfolioModal1");
+  
+  var im = document.createElement("img");
+  im.className = 'img-fluid';
+  im.src = imatges[i];
+  //catalogo.appendChild(im);
+  
+  var c = document.createElement("div");
+  c.classList.add('portfolio-item-caption', 'd-flex', 'align-items-center', 'justify-content-center', 'h-100', 'w-100');
+
+  var d = document.createElement("div");
+  d.classList.add('portfolio-item-caption-content', 'text-center', 'text-white', 'sport');
+  d.innerHTML = sports[i];
+
+  c.appendChild(d);
+  b.appendChild(c);
+  b.appendChild(im);
+  a.appendChild(b);
+  document.getElementById('catalogo').appendChild(a);
+
+  //Siguientes iteraciones
+  for (i; i < sports.length; i++) {
+    var clna = a.cloneNode(true);
+
+    var clnb = b.cloneNode(true);
     //b.attr("data-toggle", "modal");
     //b.attr("data-target", "#portfolioModal1");
     
-    var im = document.createElement("img");
-    im.className = 'img-fluid';
-    im.src = d['imatges'][i];
+    var clnim = im.cloneNode(true);
+    clnim.src = imatges[i];
     //catalogo.appendChild(im);
     
-    var c = document.createElement("div");
-    c.classList.add('portfolio-item-caption', 'd-flex', 'align-items-center', 'justify-content-center', 'h-100', 'w-100');
+    var clnc = c.cloneNode(true);
 
-    var d = document.createElement("div");
-    d.classList.add('portfolio-item-caption-content', 'text-center', 'text-white', 'sport');
-    d.innerHTML = sports[i];
+    var clnd = d.cloneNode(true);
+    clnd.innerHTML = sports[i];
 
-    c.appendChild(d);
-    b.appendChild(c);
-    b.appendChild(im);
-    a.appendChild(b);
-    document.getElementById('catalogo').appendChild(a);
+    clnc.appendChild(clnd);
+    clnb.appendChild(clnc);
+    clnb.appendChild(clnim);
+    clna.appendChild(clnb);
+    document.getElementById('catalogo').appendChild(clna);
   }
   
 }
