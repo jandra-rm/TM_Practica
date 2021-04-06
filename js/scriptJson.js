17ebf4057a9e755bcb553e7e3bd28b9dcf71ea6f
jandra_rm <alejandraribasmari@gmail.com>
2021-04-06 17:46:42 +0200

    prueba 2

--- js/scriptJson.js
+++ js/scriptJson.js
@@ -16,12 +16,13 @@ request.onreadystatechange = function () {
 
 function showSports(d) {
   const sports = d['dadesPropies']['esports'];
+  var a,b,c,d;
   for (var i = 0; i < sports.length; i++) {
     /*
-    var a = document.createElement("div");
+    a = document.createElement("div");
     a.classList.add('col-md-6', 'col-lg-4', 'mb-5');
 
-    var b = document.createElement("div");
+    b = document.createElement("div");
     b.classList.add('portfolio-item', 'mx-auto');
     //b.attr("data-toggle", "modal");
     //b.attr("data-target", "#portfolioModal1");
@@ -29,12 +30,12 @@ function showSports(d) {
     var im = document.createElement("img");
     im.className = 'img-fluid';
     im.src = d['imatges'][i];
-    catalogo.appendChild(im);
-    /*
-    var c = document.createElement("div");
+    //catalogo.appendChild(im);
+    
+    c = document.createElement("div");
     c.classList.add('portfolio-item-caption', 'd-flex', 'align-items-center', 'justify-content-center', 'h-100', 'w-100');
 
-    var d = document.createElement("div");
+    d = document.createElement("div");
     d.classList.add('portfolio-item-caption-content', 'text-center', 'text-white', 'sport');
     d.innerHTML = sports[i];
 
@@ -43,7 +44,7 @@ function showSports(d) {
     b.appendChild(im);
     a.appendChild(b);
     catalogo.appendChild(a);
-    */
+    
   }
   
 }
