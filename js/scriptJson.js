5c4dcf56679510985c5405d99abafae30cf802a0
jandra_rm <alejandraribasmari@gmail.com>
2021-04-08 14:06:23 +0200

    UPDATE dinamico
    faltaba guardar

--- js/scriptJson.js
+++ js/scriptJson.js
@@ -1,20 +1,4 @@
-function addPage(){
-    createNavBar();
-    var request = new XMLHttpRequest();
-    var url = "js/datos.json";
-    request.open("GET", url, true);
-    request.responseType = 'text';
-    request.send();
-    request.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        const deportesText = request.response; //cogemos la cadena de response
-        const deportes = JSON.parse(deportesText); //la convertimos a objeto
-        createPortada(deportes);
-        createPortfolio(deportes);
-      }
-    }
-    createFooter();
-}
+
 
 function createPortada(d){ // F
 
